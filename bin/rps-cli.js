909f0c6e14984d971f55a88524986ac861994ef7
cdeel <carson.deel@gmail.com>
2023-05-06 15:16:56 -0400

    c

---
+++ bin/rps-cli.js
@@ -0,0 +1,31 @@
+import minimist from 'minimist';
+import { rps } from "../lib/rpsls.js";
+
+var args = minimist(process.args.slice(2));
+
+if (args.h || args.help) {
+    console.log('Usage: node-rps [SHOT] \
+    Play Rock Paper Scissors (RPS) \
+                                    \
+      -h, --help      display this help message and exit \
+      -r, --rules     display the rules and exit\
+                                        \
+    Examples: \
+      node-rps        Return JSON with single player RPS result. \
+                      e.g. {"player":"rock"} \
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent. \
+                      e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    process.exit(0);
+}
+
+if (args.r || args.rules) {
+    console.log('Rules for Rock Paper Scissors: \
+        \
+    - Scissors CUTS Paper \
+    - Paper COVERS Rock \
+    - Rock CRUSHES Scissors');
+    process.exit(0);
+}
+
+console.log(rps(args._[0]));
+process.exit(0);
